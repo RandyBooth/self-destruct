6f3d53fa0038ae2a355910e5743f504037dfd8d5
Randy Booth <Randy.Booth@gmail.com>
2020-08-04 10:51:34 -0500

    Change to destructed

--- resources/views/messages/expired.blade.php
+++ resources/views/messages/expired.blade.php
@@ -4,6 +4,6 @@
 
 @section('content')
     <div class="w-full md:w-2/3 lg:w-2/3 mx-auto bg-white shadow-md rounded-sm px-8 pt-10 pb-12">
-        <p class="text-lg text-center">This note has destroyed</p>
+        <p class="text-lg text-center">This note is destructed</p>
     </div>
 @endsection
